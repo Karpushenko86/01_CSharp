7d5f4b545d95788e5a7807d445ef281ee7e9ee0d
Danya <saikirisoisor@mail.ru>
2024-02-24 02:41:54 +0500

    Add Homework solution in Lesson1:Task2

--- Lesson1/Task2/Program.cs
+++ Lesson1/Task2/Program.cs
@@ -1,16 +1,20 @@
-﻿int i1 = 5;
+﻿// Целочисленный тип
+int i1 = 5;
 int i2 = 6;
 int sum = i1 + i2;
 Console.WriteLine(sum);
 
+// Целочисленный тип 2
 long l1 = 6;
 long sum2 = l1 + i1;
 Console.WriteLine(sum2);
 
+// Вещественный тип
 double d1 = 5.5;
 double sum3 = d1 + i1;
 Console.WriteLine(sum3);
 
+// Умножение
 int product1 = i1 * i2;
 long product2 = l1 * i1;
 double product3 = i1 * d1;
@@ -18,9 +22,21 @@ Console.WriteLine(product1);
 Console.WriteLine(product2);
 Console.WriteLine(product3);
 
+
+// Целочисленное деление
 int quotient = i1 / i2;
 Console.WriteLine(quotient);
 
+// Вещественное деление
 d1 = 6;
 double quotient2 = i1 / d1;
 Console.WriteLine(quotient2);
+
+
+// Домашнее задание
+int a = 1;
+int b = 2;
+int c = 3;
+double d = 4;
+double result = (a * b) / (c + d);
+Console.WriteLine("Homework [ result ] = " + result);
